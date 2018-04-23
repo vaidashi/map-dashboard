45374c0e6e8c7e9a79123610a9e34810240775b0
Ashish Vaid <vaid50@yahoo.com>
2018-04-23 15:54:05 -0700

    minor cleanup of plotting methods

--- wickerMap.js
+++ wickerMap.js
@@ -107,11 +107,25 @@ function plotCheck(range, lat, lon) {
   if ((lat <= range["north"] && lat >= range["south"]) && (lon <= range["east"] && lon >= range["west"])) {
     var str = JSON.stringify(range);
 
-    $('#myList').append(`<li>Flight log exists within this zone  <button onclick="reportGenerator()">Click Here for the Flight Log Report </button> ${str}</li>`);
+    $('#myList').append(`
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Bounds</th>
+          <th>Report</th>
+        </tr>
+        <tr>
+          <td> ${time[0]} </td>
+          <td> ${str} </td>
+          <td><button onclick="reportGenerator()">Click Here</button></td>
+        </tr>
+        </table>
+      `)
+  } else {
+    $('#myList').empty();
+    $('#myList').append('<p>No flights within this zone </p>');
   }
-  else {
-    $('#myList').append('<li>No flights within this zone</li>');
-  }
+
 }
 
 
@@ -121,13 +135,6 @@ function plotCheckAgain(coords) {
 
   $('#myList').empty();
   $('p').empty();
-
-  // if ((startLat <= coords[0] && startLat >= coords[2]) && (startLon <= coords[1] && startLon >= coords[3])) { //north, east, south, west
-  //   $('#myList').append(`<li>Flight log exists within this zone   <button onclick="reportGenerator()">Click Here for the Flight Log Report</button> <br />  <br /> North: ${coords[0]} <br /> South: ${coords[2]}<br /> East: ${coords[1]}<br /> West: ${coords[3]}</li> <br />`);
-  // } else {
-  //   $('#myList').empty();
-  //   $('#myList').append('<li>No flights within this zone</li>');
-  // }
 
   if ((startLat <= coords[0] && startLat >= coords[2]) && (startLon <= coords[1] && startLon >= coords[3])) {
     $('#myList').append(`
@@ -146,7 +153,7 @@ function plotCheckAgain(coords) {
       `)
   } else {
     $('#myList').empty();
-    $('#myList').append('<p>No flights within this zone </p>')
+    $('#myList').append('<p>No flights within this zone </p>');
   }
 
 }
@@ -164,15 +171,5 @@ function reportGenerator() {
   $.get("https://raw.githubusercontent.com/vaidashi/map-dashboard/master/MAVIC_0103600_01.DAT.html", function(data) {
     $("p").html(data);
   })
-  // $('p').load("./MAVIC_0103600_01.DAT.html");
 
-  // $('p').append(` <br />
-  //   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus tellus nec rhoncus pretium. Fusce varius urna ante, id vulputate lorem auctor faucibus. Nullam efficitur nunc eget bibendum molestie. Aenean elementum mattis velit, ut molestie sem malesuada luctus. Cras vehicula luctus ipsum vel ornare. Duis ac sapien nec dui fringilla aliquet. Aliquam bibendum, eros at scelerisque eleifend, diam est consectetur nunc, sed dignissim ex sem eget metus.
-  //
-  //   Sed eu sollicitudin turpis, quis porta diam. Vivamus vitae gravida ex. In ac condimentum nulla. Maecenas consequat pretium lorem consequat commodo. Donec eget ipsum dignissim, ullamcorper lacus quis, consequat nisi. Etiam viverra magna ac euismod ultrices. Nullam posuere augue eget ligula maximus, in pellentesque eros tempus. Ut consectetur a massa imperdiet ullamcorper. Quisque diam lorem, rutrum ut augue in, suscipit fringilla orci.
-  //
-  //   Pellentesque commodo ante ex, rhoncus sodales neque pharetra ultricies. Nullam diam massa, tempor eu nisi sed, consectetur congue ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce sem velit, varius sit amet sem in, rutrum pellentesque arcu. Nunc in tempor erat. Nullam eu nibh aliquet, ullamcorper elit non, eleifend nulla. Aenean laoreet gravida ligula id viverra. Curabitur porta ex viverra congue feugiat. Nullam et laoreet lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas massa erat, pretium in arcu eu, maximus dignissim mi. Nam et sodales nisi. Morbi mollis mi metus, id euismod risus venenatis vitae. Nunc ullamcorper, ipsum sed mollis porttitor, nibh neque iaculis eros, viverra pharetra sem quam eu lectus. Morbi consequat pretium leo, vitae gravida magna hendrerit eu. Nunc aliquet, nisi vitae consequat ultrices, ipsum leo gravida nunc, tincidunt volutpat mauris sapien sit amet risus.
-  //
-  //   Sed eget urna odio. Nullam tristique, lectus nec porta feugiat, sapien sem sollicitudin dolor, at rutrum justo metus eget odio. Morbi porttitor rhoncus lacus, eget aliquam leo. Quisque nec urna condimentum, auctor risus id, scelerisque purus. Sed ullamcorper, nibh sit amet tempor euismod, libero dui vulputate lectus, quis condimentum urna ante id erat. Nunc in neque sit amet urna laoreet consectetur. Morbi scelerisque massa et urna dictum, id vulputate metus condimentum.
-  //   ` );
 }
